14563f149afde411595811aaa97c11188824411b
Laura Lachin <lauralachin@10-18-40-223.dynapool.wireless.nyu.edu>
2022-10-20 09:54:19 -0400

    Got calendar API working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,8 +10,10 @@ function Home() {
     const [holidayData, setHolidayData] = useState({});
     const [searchParams] = useSearchParams();
     const [country, setCountry] = useState('Canada'); 
+    let dateToday = new Date().toISOString().slice(0,10);
+    let currentYear = new Date().getFullYear();
 
-
+    // console.log(currentYear);
 // console.log("searchParams", searchParams.get("country"));
 // console.log(`https://calendarific.com/api/v2/holidays?&api_key=${HOLIDAY_API_KEY}&country=CA&year=2022`)
 
@@ -19,17 +21,40 @@ useEffect(() => {
     const countryToQuery = searchParams.get("country") || country;
     setCountry(countryToQuery);
     axios
-        .get(`https://calendarific.com/api/v2/holidays?&api_key=${HOLIDAY_API_KEY}&country=CA&year=2022`)
+        .get(`https://calendarific.com/api/v2/holidays?&api_key=${HOLIDAY_API_KEY}&country=CA&year=${currentYear}`)
         .then(function (response) {
-            setHolidayData(response.data);
-            console.log(response);
+            setHolidayData(response.data.response.holidays);
+            // console.log("response", response);
         })
         .catch(function (error) {
             console.warn(error);
             setHolidayData({});
         });
+    // axios
+    //     .get(`https://www.amdoren.com/api/currency.php?api_key=${CURRENCY_API_KEY}=USD&to=EUR`)
+    //     .then(function (response) {
+    //         setCurrencyData(response);
+    //         console.log("response", response);
+    //     })
+    //     .catch(function (error) {
+    //         console.warn(error);
+    //         setCurrencyData({});
+    //     });
     }, []);
+    // console.log("holiday data", holidayData);
+    // console.log("holiday", holidayData);
+    // console.log("holiday today", holidayData[0]);
+    // console.log("date", dateToday);
+
+    //PUT FOR LOOP IN UseMEMO!!
+    for(let i = 0; i < holidayData.length; i++) {
+        if(holidayData[i].date.iso == dateToday) {
+            console.log(holidayData[i].name);
+        }
+        else {
+            console.log("none today");
+        }
+    };
 }
-// console.log(holidayData);
 
 export default Home;
